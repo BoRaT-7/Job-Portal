797fecafbc837f3b5777b3c48461749ed86fbd9a
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-12-03 10:24:02 +0600

    commit-1

---
+++ job-portal-client/src/router/router.jsx
@@ -0,0 +1,11 @@
+// src/router/router.jsx
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world</div>,
+  },
+]);
+
+export default router;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.cjs or .js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")], // or import/ESM equivalent
+};
